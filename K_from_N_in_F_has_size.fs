8bc47a905c89420d63155a7b72eedca446c6eee8
Renegatto <46404781+Renegatto@users.noreply.github.com>
2020-05-09 22:35:46 +0300

    pure/impure, responsibilities segregation

--- K_from_N_in_F_has_size.fs
+++ K_from_N_in_F_has_size.fs
@@ -1,12 +1,11 @@
 module K_from_N_in_F_has_size
 open Infrastructure
-//tools
-let T x y = x
-let F x y = y
-let showme s x =
-    printfn "%A%A" s x
-    x
-    
+//propability estimation
+let simulate (times:int) (result_generator: unit -> bool IO): (int * float) IO =
+    List.map (fun _ -> result_generator ()) [1..times]
+    |> List.map (IO.map (fun x -> if x then 1 else 0))
+    |> List.reduce (IO.map2 (+))
+    |> IO.map (fun successes -> successes, (float successes / float times) )  
 //randoms
 let shuffle (rng: System.Random) (xs: 'a list): list<'a>*System.Random = 
     List.sortBy (fun x -> rng.Next (0, List.length xs)) xs, rng
@@ -20,8 +19,15 @@ let isHypothesisRight (size:int) (k:int Set) (m: int Set): bool =
     Set.intersect k m
     |> Set.count
     |> fun x -> x > size
-//usage
-let checkHypothesis (n_length: int) (k_length: int) (m_length: int) (s_length: int): bool IO =
+//logic
+let isHypothesisRight (size:int) (k:int Set, m: int Set): bool =
+    Set.intersect k m
+    |> Set.count
+    |> fun x -> x > size
+
+let prepareData (n_length: int) (k_length: int) (m_length: int) ()
+    : (Set<int>*Set<int>) IO =
+
     let rng: System.Random = new System.Random ()
 
     let n: int list = [1..n_length]
@@ -31,10 +37,11 @@ let checkHypothesis (n_length: int) (k_length: int) (m_length: int) (s_length: i
 
     let k: Set<int> IO = IO.map (fst>>Set) k'
     let m: Set<int> IO = IO.map (fst>>Set) m'
-    IO.map2 (isHypothesisRight s_length) k m
 
-let simulate (times:int): float IO =
-    List.map (fun _ -> checkHypothesis 20 5 7 3) [1..times]
-    |> List.map (IO.map (fun x -> if x then 1 else 0))
-    |> List.reduce (IO.map2 (+)) |> showme "successes ="
-    |> IO.map (fun successes -> float successes / float times )
+    IO.map2 (fun x y -> x,y) k m
+//usage
+let simul_K_from_N_in_M_from_N_has_size (): unit IO =
+    (IO.map (isHypothesisRight 3) << prepareData 20 5 7)
+    |> simulate 200000
+    |> IO.map (fun (successes, propability) -> 
+        printfn "Simulation, successes/all = %A/%A, P(x) = %A" successes 200000 propability)
