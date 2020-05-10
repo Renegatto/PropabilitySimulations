87f628921b1a70a05d21bd2c6b67482bb7359dca
Renegatto <edittorr@mail.ru>
2020-05-10 08:39:43 +0300

    test call from main

--- Program.fs
+++ Program.fs
@@ -1,8 +1,10 @@
 ﻿// Learn more about F# at https://fsharp.org
 // See the 'F# Tutorial' project for more help.
+open PropabilityCalculations
 
 [<EntryPoint>]
 let main argv =
-    //
+    printfn "%A" <| calculate 20. 5. 7. 3
     printfn "%A" argv
+    System.Console.ReadLine () |> ignore
     0 // return an integer exit code
